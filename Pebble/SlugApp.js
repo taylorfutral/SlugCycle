1d8b03aa42c30f1227f7026a9282cd93fc6b0540
taylorfutral <taylorfutral@eduroam-169-233-227-82.ucsc.edu>
2016-01-30 17:52:30 -0800

    I added a Pebble app to explain SlugCycle use procedure.

---
+++ Pebble/SlugApp.js
@@ -0,0 +1,62 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+var Vector2 = require('vector2');
+
+var main = new UI.Card({
+  title: 'SlugCycle.js',
+  icon: 'images/SlugCycle2.png',
+  subtitle: 'Welcome to SlugCycle!',
+  body: 'Press any button to continue.',
+  subtitleColor: 'indigo', // Named colors
+  bodyColor: '#9a0036' // Hex colors
+});
+
+main.show();
+
+main.on('click', 'up', function(e) {
+  var menu = new UI.Menu({
+    sections: [{
+      items: [{
+        title: 'SlugCycle.js',
+        icon: 'images/SlugCycle2.png',
+        subtitle: 'Welcome'
+      }, {
+        title: 'To get started',
+        subtitle: 'Make sure you exit'
+      }]
+    }]
+  });
+  menu.on('select', function(e) {
+    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    console.log('The item is titled "' + e.item.title + '"');
+  });
+  menu.show();
+});
+
+main.on('click', 'select', function(e) {
+  var wind = new UI.Window({
+    fullscreen: true,
+  });
+  var textfield = new UI.Text({
+    position: new Vector2(0, 65),
+    size: new Vector2(144, 30),
+    font: 'gothic-24-bold',
+    text: 'Welcome to SlugCycle!',
+    textAlign: 'center'
+  });
+  wind.add(textfield);
+  wind.show();
+});
+
+main.on('click', 'down', function(e) {
+  var card = new UI.Card();
+  card.title('SlugCycle');
+  card.subtitle('Welcome to SlugCycle!');
+  card.body('Please add your number to the SlugCycle app to and then exit to begin');
+  card.show();
+});
