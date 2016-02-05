1a2fe6a71158f3d232b2b840a4d890e5c236a44e
mrunderhill89 <mrunderhill89@gmail.com>
2016-02-05 15:14:45 -0800

    Switched the number back over to Taylor for the Inboard meeting.

--- Meteor/client/map_template.js
+++ Meteor/client/map_template.js
@@ -1,4 +1,4 @@
-﻿/** Polyfill: Converts numeric degrees to radians */
+/** Polyfill: Converts numeric degrees to radians */
 if (typeof(Number.prototype.toRadians) === "undefined") {
   Number.prototype.toRadians = function() {
     return this * Math.PI / 180;
@@ -273,7 +273,7 @@ Template.map.onCreated(function() {
 			};
         }).bufferingThrottle(1000.0).onValue(function(args){
             console.log("sending SMS from client:",args);
-            Meteor.call("send_SMS", "8317138143", "Hazard approx. "+args.distance+" feet away.");            
+            Meteor.call("send_SMS", "9253213959", "Hazard approx. "+args.distance+" feet away.");            
         });
         //Meteor.call("send_SMS", "8313255847", marker.position.toString());
     });
